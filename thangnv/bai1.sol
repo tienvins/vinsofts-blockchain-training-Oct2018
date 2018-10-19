d1a9efaa7e551303da62844b87d9c628517cc686
nvt1904 <nvt1904@gmail.com>
2018-10-19 15:13:12 +0700

    update

--- thangnv/bai1.sol
+++ thangnv/bai1.sol
@@ -1,119 +1,114 @@
 pragma solidity ^0.4.25;
 
-//modify by aris-vu
-//overview: it's looking so good, but, i've something i recommended for you. see below
-
 contract Bank{
-    //first, using English for code, comment
-    string _tenbank; //need to be edit
-    uint _tongtien; //need to be edit
-    address _owner; //with local variable, don't use _
+
+    string name;
+    uint totalMoney;
+    address owner;
     
-    constructor(string ten) {
-        _tenbank=ten;
-        _tongtien=1000;
-        _owner=msg.sender;
+    constructor(string _name) {
+        name=_name;
+        totalMoney=1000;
+        owner=msg.sender;
     }
     
-    //with function name, it should be changeName, without _
-    //should we using modifier to replace require?
-    function change_name(string ten ) {
-        require(_owner==msg.sender,"Bạn không phải chủ ngân hàng");
-        _tenbank=ten; 
+    function changeName(string _name ) {
+        require(owner==msg.sender,"Bạn không phải chủ ngân hàng");
+        name=_name; 
     }
     
-    // it should be replace by public variable
-    function get_info() returns(string,address,uint){
-       return(_tenbank,_owner,_tongtien);
+    function getInfo() public returns(string,address,uint){
+       return(name,owner,totalMoney);
     }
 }
 
 interface Action{
-    function gui_tien(address onwernhan, uint sotien) returns(address, address,uint,uint,uint);
-    function rut_tien(uint sotien) returns(address, uint, uint, uint );
+    function sendMoney(address _toonwer, uint _money) returns(address, address,uint,uint,uint);
+    function rut_tien(uint _money) returns(address, uint, uint, uint );
 }
 
 contract Bank2 is  Bank, Action {
     
-    mapping(address => Onwer) OwnerToUser; //first characters should be lowercase
+    mapping(address => Onwer) ownerToUser;
     
-    //it should not be using _ for variable in the struct, see local variable
     struct Onwer{
-        string _ten;
-        address _id;
-        uint _sodu;
+        string name;
+        address id;
+        uint money;
     }
     
-    Onwer[] _Onwer; //with array, variable name should have 's', e.g: owners
+    Onwer[] onwers;
     
     struct History{
-        address  _from;  
-	    address  _to; 
-	    uint  _value;
-	    uint  _time;
+        address  from;  
+	    address  to; 
+	    uint  money;
+	    uint  time;
     }
     
-    History[] _History;
+    History[] historys;
 
     
     constructor(string n) Bank (n){
     }
     
-    function dang_ky(string ten) returns(string,address,uint){
-        Onwer memory onwer = Onwer(ten, msg.sender,1000);
-        _Onwer.push(onwer);
-        OwnerToUser[msg.sender]=onwer;
-        _tongtien+=1000;
-        return (onwer._ten,onwer._id,onwer._sodu);
+    function register(string _name) returns(string,address,uint){
+        Onwer memory onwer = Onwer(_name, msg.sender,1000);
+        onwers.push(onwer);
+        ownerToUser[msg.sender]=onwer;
+        totalMoney+=1000;
+        return (onwer.name,onwer.id,onwer.money);
     }
     
-    function get_info_onwer() returns (string,address,uint){
-        Onwer memory onwer = OwnerToUser[msg.sender];
-        return (onwer._ten,onwer._id,onwer._sodu);
+    function getInfoOnwer() returns (string,address,uint){
+        Onwer memory onwer = ownerToUser[msg.sender];
+        return (onwer.name,onwer.id,onwer.money);
     }
     
-    modifier check_acc(address onwer) {
-        require(OwnerToUser[msg.sender]._id==msg.sender,"Không có tài khoản");
+    modifier checkAcc(address onwer) {
+        require(ownerToUser[msg.sender].id==msg.sender,"Không có tài khoản");
         _;
     }
     
-    modifier check_tongtien(uint sotien) {
-        require(_tongtien>=sotien,"Ngân hàng không đủ tiền");
+    modifier checkTotalMoney(uint _money) {
+        require(totalMoney>=_money,"Ngân hàng không đủ tiền");
         _;
     }
     
-    modifier check_sodu(address onwer,uint sotien) {
-        require(OwnerToUser[msg.sender]._sodu>=sotien,"Số dư không đủ");
+    modifier checkMoney(address onwer,uint _money) {
+        require(ownerToUser[msg.sender].money>=_money,"Số dư không đủ");
         _;
     }
     
-    event da_gui(address onwergui, address onwernhan,uint sotien);
+    event da_gui(address onwergui, address _toonwer,uint _money);
     
-    function gui_tien(address onwernhan, uint sotien) check_acc(msg.sender) check_tongtien(sotien) check_sodu(msg.sender,sotien) returns(address, address,uint,uint,uint){
-        OwnerToUser[msg.sender]._sodu-=sotien;
-        OwnerToUser[onwernhan]._sodu+=sotien;
-        emit da_gui(msg.sender, onwernhan, sotien);
-        _History.push(History(msg.sender,onwernhan,sotien,now));
-        return (OwnerToUser[msg.sender]._id,OwnerToUser[onwernhan]._id,sotien,OwnerToUser[onwernhan]._sodu,now);
-    }
-    function rut_tien(uint sotien) check_acc(msg.sender) check_tongtien(sotien) check_sodu(msg.sender,sotien) returns(address, uint,uint,uint){
-        OwnerToUser[msg.sender]._sodu-=sotien;
-        _tongtien-=sotien;
-        return (OwnerToUser[msg.sender]._id,sotien,OwnerToUser[msg.sender]._sodu,now);
+    function sendMoney(address _toonwer, uint _money) checkAcc(msg.sender) checkTotalMoney(_money) checkMoney(msg.sender,_money) returns(address, address,uint,uint,uint){
+        ownerToUser[msg.sender].money-=_money;
+        ownerToUser[_toonwer].money+=_money;
+        emit da_gui(msg.sender, _toonwer, _money);
+        historys.push(History(msg.sender,_toonwer,_money,now));
+        return (ownerToUser[msg.sender].id,ownerToUser[_toonwer].id,_money,ownerToUser[_toonwer].money,now);
     }
     
-    function get_onwer() returns(address[]){
-        require(_owner==msg.sender,"Bạn không phải chủ ngân hàng");
-        address[] memory ds = new address[](_Onwer.length);
-        for(uint i=0; i<_Onwer.length;i++){
-            ds[i]=(_Onwer[i]._id);
+    function rut_tien(uint _money) checkAcc(msg.sender) checkTotalMoney(_money) checkMoney(msg.sender,_money) returns(address, uint,uint,uint){
+        ownerToUser[msg.sender].money-=_money;
+        totalMoney-=_money;
+        return (ownerToUser[msg.sender].id,_money,ownerToUser[msg.sender].money,now);
+    }
+    
+    function getOnwers() returns(address[]){
+        require(owner==msg.sender,"Bạn không phải chủ ngân hàng");
+        address[] memory ds = new address[](onwers.length);
+        for(uint i=0; i<onwers.length;i++){
+            ds[i]=(onwers[i].id);
         }
         return ds;
     }
-    function get_history_onwer() check_acc(msg.sender) returns(uint){
+
+    function getHistoryOnwers() checkAcc(msg.sender) returns(uint){
         uint sum=0;
-        for(uint i=0; i<_History.length;i++){
-            if(_History[i]._from==msg.sender){
+        for(uint i=0; i<historys.length;i++){
+            if(historys[i].from==msg.sender){
                 sum+=1;
             }
         }
