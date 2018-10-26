6e17aa1fb1e6ddbc4f4ce1637c2d98351580f81d
vinsofts-trongnd <41321191+vinsofts-trongnd@users.noreply.github.com>
2018-10-26 15:52:12 +0700

    Update ico.sol

--- trongnd/token/ico.sol
+++ trongnd/token/ico.sol
@@ -1,106 +1,155 @@
 pragma solidity ^0.4.25;
+import "./vin-token.sol";
 
-import "./test-token.sol";
+library SafeMath {
+    function add(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function sub(uint a, uint b) internal pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+    function mul(uint a, uint b) internal pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+    function div(uint a, uint b) internal pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
 
-contract ICOVinTokenSale is VinToken{
+
+contract ICOTokenSale is ERC20Token{
+    using SafeMath for uint256;
     
-    uint public timeFrom1 = now;
-    uint public timeTo1 = now+ 1 days; //thời gian mở bán 1
+    address public tokenOnwer; //the address receive ether
+    uint private price = 1; // gia ban token
     
-    uint public timeFrom2 = now+7 days;
-    uint public timeTo2 = now+ 8 days; //thời gian mở bán 2
+    uint public tokenSale1;//max tokens for public-sale
+    uint public tokenSale2;//max tokens for public-sale
+    uint public tokenSale3;//max tokens for public-sale
+    uint public tokenSale4;//max tokens for public-sale
     
-    uint public timeFrom3 = now+14 days;
-    uint public timeTo3 = now+ 15 days; //thời gian mở bán 3
+    uint public phasePublicSale1_From = now;
+    uint public phasePublicSale1_To = now + 3 days;
+    uint public preSaleTokenSold1 = 0;  // token ban dc lan 1
+
+    uint public phasePublicSale2_From = now + 4 days;
+    uint public phasePublicSale2_To = now + 5 days;
+    uint public preSaleTokenSold2 = 0;  // token ban dc lan 2
+
+    uint public phasePublicSale3_From = now + 6 days;
+    uint public phasePublicSale3_To = now + 7 days;
+    uint public preSaleTokenSold3 = 0;  // token ban dc lan 3
     
-    uint public timeFrom4 = now+21 days;
-    uint public timeTo4 = now+ 22 days; //thời gian mở bán 4
+    uint public phasePublicSale4_From = now;
+    uint public phasePublicSale4_To = now + 1 days;
+    uint public preSaleTokenSold4 = 0;  // token ban dc lan 4
     
-    uint public maxToken1 =5000; //tổng số token có thể bán
-    uint public buyToken1 =0;    //số token đã bán
-    uint public totalEther1=0;
-    
-    uint public maxToken2 =10000; 
-    uint public buyToken2 =0;  
-    uint public totalEther2=0;
-    
-    uint public maxToken3 =15000; 
-    uint public buyToken3 =0; 
-    uint public totalEther3=0;
-    
-    uint public maxToken4 =20000; 
-    uint public buyToken4 =0; 
-    uint public totalEther4=0;
-    
-    address public tokenOnwer; //tài hoản tạo ICOVinTokenSale
-    
-    constructor() VinToken(){
-        tokenOnwer=msg.sender;
+    uint public preSaleTokenSold = 0;  // token ban dc
+	  
+	constructor(uint _initialSupply,string _nameCoin, string _symbol, uint _decimals) ERC20Token(_initialSupply, _nameCoin, _symbol, _decimals) public{
+	    tokenOnwer = msg.sender;
+	    tokenSale1 = 10000 * 10**decimals;
+	    tokenSale2 = 20000 * 10**decimals;
+	    tokenSale3 = 30000 * 10**decimals;
+	    tokenSale4 = 40000 * 10**decimals;
+	    totalSupply = tokenSale1 + tokenSale2 + tokenSale3 + tokenSale4;
+	}
+	
+	struct Buyer {
+        uint totalETH; // total eth of investor to buy token
+        uint totalTokens; // total token of investor
     }
     
-    function getStep() public view returns(uint){
-        if(now>=timeFrom1 || now<=timeTo1)
-            return 1;
-        else if(now>=timeFrom2 || now<=timeTo2)
-            return 2;
-        else if(now>=timeFrom3 || now<=timeTo3)
-            return 3;
-        if(now>=timeFrom4 || now<=timeTo4)
-            return 4;
-        return 0;
-    }
-    
-    function addBuy(uint tokens,uint ethers) internal {
-        if(getStep()==1){
-            buyToken1+=tokens;
-            totalEther1+=ethers;
-        } else if(getStep()==2){
-            buyToken2+=tokens;
-            totalEther3+=ethers;
-        } else if(getStep()==3){
-            buyToken3+=tokens;
-            totalEther3+=ethers;
-        } else{
-            buyToken4+=tokens;
-            totalEther4+=ethers;
-        }
-    }
-    
-    function buyVinToken() checkTime() checkEther() checkNumberTokenICO() payable returns(bool) {
-        uint tokens= msg.value/price;
-        tokenOnwer.transfer(msg.value);
-        toOnwer[tokenOnwer].tokenOnwer-=tokens;
-        toOnwer[msg.sender].tokenOnwer+=tokens;
-        addBuy(tokens,msg.value/1 ether);
-        return true;
-    }
-    
-    function changePrice(uint priceNew) checkOnwer(){
-        price=priceNew;
-    }
-    
-    modifier checkTime(){
-        require(getStep()!=0,"Không trong thời gian mở bán");
-        _;
-    }
-    
-    modifier checkNumberTokenICO() {
-        require(getStep()==1&&msg.value/price<=maxToken1-buyToken1
-                ||getStep()==2&&msg.value/price<=maxToken2-buyToken2
-                ||getStep()==3&&msg.value/price<=maxToken3-buyToken3
-                ||getStep()==4&&msg.value/price<=maxToken4-buyToken4
-            );
-            _;
-    }
-    
-    modifier checkEther(){
-         require(msg.value>=price);
-         require(msg.value%price==0);
-        _;
-    }
-    
-    modifier checkOnwer(){
-        require(msg.sender==tokenOnwer,"Bạn không phải chủ token");
-        _;
-    }
+	mapping (address => Buyer) public buyers;
+	
+	function getSteep() public view returns(uint) {
+		if(now<phasePublicSale1_To){
+			if(now>=phasePublicSale1_From)
+				return 1;
+		} else if(now<phasePublicSale2_To){
+			return 2;
+		} else if(now<phasePublicSale3_To){
+			return 3;
+		} else if(now<phasePublicSale4_To){
+			return 4;
+		}
+		return 0;
+	}
+	
+	function ethtoToken(uint _value) returns(uint){
+	    require(_value >= 1 ether,"Mua toi thieu la 1 ether");
+	    _value = _value/10**18;
+	    uint boughtTokens = _value.mul(getPrice1Token());
+	    return boughtTokens;
+	}
+	
+	function getPrice1Token() public view returns (uint){
+	    uint icoSteep = getSteep(); 
+	    uint price2 = 1;
+	    if(icoSteep == 1){
+	        price2 = price.mul(10);
+	    }else if(icoSteep == 2){
+	        price2 = price.mul(5);
+	    }else if(icoSteep == 3){
+	        price2 = price.mul(2);
+	    }if(icoSteep == 4){
+	        price2 = price.mul(1);
+	    }
+	    return price2;
+	}
+	
+	function getNumberTokenConLai() public view returns (uint){
+	    uint icoSteep = getSteep(); 
+	    if(icoSteep == 1){
+	        return (tokenSale1.sub(preSaleTokenSold1));
+	    }else if(icoSteep == 2){
+	        return (tokenSale2.sub(preSaleTokenSold2));
+	    }else if(icoSteep == 3){
+	        return (tokenSale3.sub(preSaleTokenSold3));
+	    }else if(icoSteep == 4){
+	        return (tokenSale4.sub(preSaleTokenSold4));
+	    }
+	}
+	function buy() payable public returns(bool){
+	    
+	    uint ethReceive = msg.value/10**18; // get ether investor send
+	    uint icoSteep = getSteep(); 
+	    uint boughtTokens = ethtoToken(msg.value); // caculate token 
+	    
+	    if(icoSteep == 1){
+	        require(preSaleTokenSold1.add(boughtTokens) <= tokenSale1,"khong duoc phep mua qua so luong token lan 1");
+    	        buyers[msg.sender].totalETH = buyers[msg.sender].totalETH.add(ethReceive);
+    		    buyers[msg.sender].totalTokens = buyers[msg.sender].totalTokens.add(boughtTokens);
+    		    preSaleTokenSold1 = preSaleTokenSold1.add(boughtTokens);
+    		    preSaleTokenSold = preSaleTokenSold.add(boughtTokens);
+	    }else if(icoSteep == 2){
+	        require(preSaleTokenSold2.add(boughtTokens) <= tokenSale2,"khong duoc phep mua qua so luong token lan 2");
+    	        buyers[msg.sender].totalETH = buyers[msg.sender].totalETH.add(ethReceive);
+    		    buyers[msg.sender].totalTokens = buyers[msg.sender].totalTokens.add(boughtTokens);
+    		    preSaleTokenSold2 = preSaleTokenSold2.add(boughtTokens);
+    		    preSaleTokenSold = preSaleTokenSold.add(boughtTokens);
+	    }else if(icoSteep == 3){
+	        require(preSaleTokenSold3.add(boughtTokens) <= tokenSale3,"khong duoc phep mua qua so luong token lan 3");
+    	        buyers[msg.sender].totalETH = buyers[msg.sender].totalETH.add(ethReceive);
+    		    buyers[msg.sender].totalTokens = buyers[msg.sender].totalTokens.add(boughtTokens);
+    		    preSaleTokenSold3 = preSaleTokenSold3.add(boughtTokens);
+    		    preSaleTokenSold = preSaleTokenSold.add(boughtTokens);
+	    }else if(icoSteep == 4){
+	        require(preSaleTokenSold4.add(boughtTokens) <= tokenSale4,"khong duoc phep mua qua so luong token lan 4");
+    	        buyers[msg.sender].totalETH = buyers[msg.sender].totalETH.add(ethReceive);
+    		    buyers[msg.sender].totalTokens = buyers[msg.sender].totalTokens.add(boughtTokens);
+    		    preSaleTokenSold4 = preSaleTokenSold4.add(boughtTokens);
+    		    preSaleTokenSold = preSaleTokenSold.add(boughtTokens);
+	    }
+	    tokenOnwer.transfer(msg.value);  // chu token nhan tien tu ng gui
+	    return true;
+	}
+	
+	function () public {
+	    buy();
+	}
 }
