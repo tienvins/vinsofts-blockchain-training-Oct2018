9e7d7b4c65255e0347f84f8ea9a4c84df0e66245
tupa <tupa@vinsofts.net>
2018-10-31 10:35:25 +0700

    edit loi

--- tupa/cacuoc/cacuoc.sol
+++ tupa/cacuoc/cacuoc.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.25;
 contract CC{
     address owner = this;
     uint totalPeople = 3;
+    uint ethBet = 1 ether;
     struct User{    
         uint number;
         address addres;
@@ -13,21 +14,26 @@ contract CC{
         _;
     }
     modifier checkNumber(uint _number){
-        require(_number < totalPeople,"Số đc chon từ 1->10");
+        require(_number <= totalPeople && _number > 0,"Số đc chon từ 1->3");
         _;
     }
+    
     function chooseNumber(uint _number) checkBalance checkNumber(_number) public payable returns(address _addressWin){
+        if(msg.value > ethBet){
+            msg.sender.transfer(msg.value - ethBet);
+        }
         users.push(User(_number,msg.sender));
         if(users.length == totalPeople){
             return random();
         }
     }
-    function random() private view returns(address _addressWin){
-        uint random = uint(keccak256(now)) % totalPeople;
+    
+    function random() private returns(address _addressWin){
+        uint random = uint(keccak256(now)) % totalPeople + 1;
         for(uint i=0; i<users.length;i++){
             if(users[i].number == random){
                 _addressWin = users[i].addres;
-                users[i].addres.transfer(owner.balance);
+                _addressWin.transfer(owner.balance);
             }
         }
     }
