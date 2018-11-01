811e1f5261b1b7526995cfc38c8addb07ed2dd4a
TrienTT <trientt@vinsofts.net>
2018-11-01 11:39:06 +0700

    trientt update bai ca cuoc va truy xuat thong tin

--- Trientt/TruyXuatThongTin.sol
+++ Trientt/TruyXuatThongTin.sol
@@ -1,22 +1,21 @@
 pragma solidity ^0.4.23;
 pragma experimental ABIEncoderV2;
-contract HoSo{
-    struct CaNhan{
+contract file{
+    struct personal{
         string name;
         uint age;
         string sex;
     }
-    //mapping (uint => CaNhan) cn;
-    CaNhan[] lisPeople;
+    personal[] lisPeople;
     function setPeople(string _name, uint _age, string _sex, uint count) public{
-        lisPeople.push(CaNhan(_name,_age,_sex));
+        lisPeople.push(personal(_name,_age,_sex));
     }
     function getPeoplebyId(uint id) public view returns(string _name, uint _age, string _sex, uint count){
             _name = lisPeople[id].name;
             _sex = lisPeople[id].sex;
             _age = lisPeople[id].age;
     }
-    function getAllPeople() public view returns(CaNhan[]){
+    function getAllPeople() public view returns(personal[]){
         return lisPeople;
     }
     
