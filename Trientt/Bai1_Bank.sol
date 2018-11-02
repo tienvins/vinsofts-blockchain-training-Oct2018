4aeb37f80aa5678bdb0537045fa4a940f25061ac
TrienTT <trientt@vinsofts.net>
2018-11-02 15:35:38 +0700

    update bank

--- Trientt/Bai1_Bank.sol
+++ Trientt/Bai1_Bank.sol
@@ -1,4 +1,4 @@
-﻿pragma solidity ^0.4.23;
+pragma solidity ^0.4.23;
 pragma  experimental ABIEncoderV2;
 contract Bank{
     string  name;
@@ -70,6 +70,7 @@ contract Bank2 is Bank,func{
         return listUser;
     }
     //chức năng gửi tiền cho 1 tài khoản khác and  save history
+    event sended(address _from, address _to,uint _money);
     function sendTo(address _to, uint _amount) public returns (bool){
          require(checkExit[msg.sender] !=0);   // chỉ khi đăng ký ms chuyển đc tiền
          require(listUser[_addToIndex[msg.sender]].balancs > _amount );
@@ -78,11 +79,13 @@ contract Bank2 is Bank,func{
          listUser[_addToIndex[msg.sender]].balancs -=_amount;  // trừ tiền trong list user để get ra user luôn chính 
          listUser[_addToIndex[_to]].balancs +=_amount;
          //listHis.push(history("send money",msg.sender,_to,_amount,now)); // save history
-         listHisByAdres[msg.sender].push(history("send money",msg.sender,_to,_amount,now));
-         listHisByAdres[_to].push(history("receive money",msg.sender,_to,_amount,now));
+         listHisByAdres[msg.sender].push(history("send money",msg.sender,_to,_amount,now)); //thêm  vào listHisByAdres theo adress
+         listHisByAdres[_to].push(history("receive money",msg.sender,_to,_amount,now)); //thêm  vào listHisByAdres theo adress
+         emit sended(msg.sender,_to,_amount);
         return true;
     }
     //rút tiền  and  save history
+    event received(address _from,uint _money);
     function receive(uint _amount) public  returns (bool){
         require(checkExit[msg.sender] !=0);   // chỉ khi đăng ký ms chuyển đc tiền
         require(msg.sender != owner);
@@ -91,6 +94,7 @@ contract Bank2 is Bank,func{
         listUser[_addToIndex[msg.sender]].balancs -=_amount;
         //listHis.push(history("receive money",msg.sender,address(0),_amount,now)); // save history
         listHisByAdres[msg.sender].push(history("receive money",msg.sender,address(0),_amount,now));
+        emit received(msg.sender,_amount);
         return true;
     }
     function getHis() public view returns(history[]){
