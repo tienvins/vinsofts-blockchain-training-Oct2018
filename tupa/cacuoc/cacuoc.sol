2e178d1766b503540a5bd5a68891b749678d8d45
tupa <tupa@vinsofts.net>
2018-11-05 15:00:08 +0700

    edit bt

--- tupa/cacuoc/cacuoc.sol
+++ tupa/cacuoc/cacuoc.sol
@@ -1,40 +1,78 @@
-pragma solidity ^0.4.25;
-contract CC{
-    address owner = this;
-    uint totalPeople = 3;
-    uint ethBet = 1 ether;
-    struct User{    
-        uint number;
-        address addres;
-    }
-    User[] users;
+pragma solidity ^0.4.23;
+
+contract Betting {
+    address public owner;
+    uint public totalPeople = 10;
+    uint public ethBet = 1 ether;
+    uint public totalAmount;
+    uint public numberPeopleBet = 0;
+    uint public winner;
+    address[] public players;
     
-    modifier checkBalance(){
-        require(msg.sender.balance > msg.value, "Bạn ko đủ tiền để cược");
+    mapping(uint => address[]) numberToPlayers;
+    mapping(address => uint) playerToNumber;
+    
+    modifier check(uint _number){
+        require(msg.sender.balance > msg.value,'Số dư của bạn ko đủ');
+        require(0 < _number && _number <= totalPeople, 'Số đc chọn từ 1->10');
+        require(playerToNumber[msg.sender] == 0, 'Bạn chỉ đc chọn một lần');
         _;
     }
-    modifier checkNumber(uint _number){
-        require(_number <= totalPeople && _number > 0,"Số đc chon từ 1->3");
+    
+    modifier checkOwner(){
+        require(owner == msg.sender,'Bạn ko phải là chủ');
         _;
     }
+    constructor(){
+        owner = msg.sender;
+    }
     
-    function chooseNumber(uint _number) checkBalance checkNumber(_number) public payable returns(address _addressWin){
+    function bet(uint _number) public payable check(_number){
         if(msg.value > ethBet){
             msg.sender.transfer(msg.value - ethBet);
         }
-        users.push(User(_number,msg.sender));
-        if(users.length == totalPeople){
-            return random();
+        playerToNumber[msg.sender] = _number;
+        players.push(msg.sender);
+        numberToPlayers[_number].push(msg.sender);
+        numberPeopleBet += 1;
+        totalAmount += msg.value;
+        if(numberPeopleBet == totalPeople){
+            run();
         }
     }
-    
-    function random() private returns(address _addressWin){
-        uint random = uint(keccak256(now)) % totalPeople + 1;
-        for(uint i=0; i<users.length;i++){
-            if(users[i].number == random){
-                _addressWin = users[i].addres;
-                _addressWin.transfer(owner.balance);
+
+    function run() internal {
+        uint winnerNumber = random();
+        address[] memory winners = numberToPlayers[winnerNumber];
+        if(winners.length > 0){
+            uint winnerEthBet = totalAmount / winners.length;
+            for (uint i = 0; i < numberToPlayers[winnerNumber].length; i++) {
+                winners[i].transfer(winnerEthBet);
             }
         }
+        winner = winnerNumber;
+        reset();
     }
+        
+    function random() internal view returns(uint) {
+        return (uint(keccak256(now)) % totalPeople + 1);
+    }
+    
+    function reset() internal {
+        for (uint i=0; i <= totalPeople; i++){
+            numberToPlayers[i].length = 0;
+        }
+        for (uint j=0; j < players.length; j++){
+            playerToNumber[players[j]] = 0;
+        }
+        
+        players.length = 0;
+        totalAmount = 0;
+        numberPeopleBet = 0;
+    }
+    
+    function kill() checkOwner public {
+        selfdestruct(owner);
+    }
+
 }
--- tupa/vintoken.sol
+++ tupa/vintoken.sol
@@ -1,72 +1,130 @@
 pragma solidity ^0.4.25;
 
+library SafeMath {
+  function mul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint a, uint b) internal returns (uint) {
+    uint c = a / b;
+    return c;
+  }
+
+  function sub(uint a, uint b) internal returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
+    assert(c >= a);
+    return c;
+  }
+
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a < b ? a : b;
+  }
+
+  function assert(bool assertion) internal {
+    if (!assertion) {
+      throw;
+    }
+  }
+}
+
 contract Token {
-    uint public totalSupply;
+
+    function totalSupply() constant returns (uint256 supply) {}
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+}
+contract StandardToken is Token {
     
-    struct User{
-        address owner;
-        uint amount;
-    }
+    using SafeMath for uint;
     
-    User[] users;
-    
-    mapping (address => User) toUser;
-    mapping (address => mapping(address => uint)) allowed;
-    
-    modifier checkAcc(){
-        require(toUser[msg.sender].owner == msg.sender,"Bạn chưa có tài khoản");
-        _;
-    }
-    modifier checkRigister(){
-        require(toUser[msg.sender].owner != msg.sender,"Bạn đã có tài khoản");
-        _;
-    }
-    modifier checkMoney(uint _money){
-        require(toUser[msg.sender].amount >= _money, "Bạn ko đủ tiền");
-        _;
-    }
-    modifier checkAllowance(address _from,uint _money){
-        require(allowance(_from,msg.sender) >= _money,"Số token được ủy quyền  cho bạn nhỏ hơn số  bạn đang chọn ");
+    modifier checkAllowance(address _from, uint _value){
+        require(allowance(_from,msg.sender) >= _value, 'Số token đc ủy quyền cho bạn nhỏ hơn số bạn đang chọn');
         _;
     }
     
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        require(balances[msg.sender] >= _value && _value > 0,'số dư bạn ko đủ');
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to]        = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) checkAllowance(_from, _value) returns (bool success) {
+        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0, 'số dư bạn ko đủ');
+        balances[_to]              = balances[_to].add(_value);
+        balances[_from]            = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+}
+
+contract ERC20Token is StandardToken {
+
+    function () {
+        throw;
+    }
+
+    string public name;                   
+    uint8 public decimals;                
+    string public symbol;                 
+    string public version = 'H1.0';       
+
     constructor() {
-        totalSupply=1000;
-    }
-    
-    function register() checkRigister public {
-        User memory user = User(msg.sender,100);
-        users.push(user);
-        toUser[msg.sender] = user;
-    }
-    
-    function balanceOf(address _owner) checkAcc public returns(uint){
-        return toUser[_owner].amount;
-    }
-    
-    function balance() checkAcc public view returns(uint){
-        return toUser[msg.sender].amount;
-    }
-    
-    function Transfer(address _to, uint _money) checkAcc checkMoney(_money) public returns(bool success){
-        toUser[msg.sender].amount -= _money;
-        toUser[_to].amount += _money;
-        return true;
-    }
-    
-    function TransferFrom(address _from, address _to, uint _money) checkAcc checkAllowance(_from,_money) public returns(bool success){
-        toUser[_from].amount -= _money;
-        allowed[_from][msg.sender] -= _money; 
-        toUser[_to].amount += _money;
-        return true;
-    }
-    
-    function approve(address _spender, uint _money) checkAcc checkMoney(_money) public returns (bool success){
-        allowed[msg.sender][_spender] = _money;
-        return true;
-    }
-    
-    function allowance(address _owner, address _spender) checkAcc public returns (uint) {
-        return allowed[_owner][_spender];
+        balances[msg.sender] = 100000;               
+        totalSupply = 100000;                        
+        name = "TuCoinCash";            
+        decimals = 0;                                
+        symbol = "TCH";                             
     }
 }
