1703dd1710f3a950fd493f0f623237d4e06785df
vinsofts-trongnd <trongnd@vinsofts.net>
2018-11-05 12:01:52 +0700

    caccuoc fix

--- trongnd/b3/b3.sol
+++ trongnd/b3/b3.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.4.25;
 contract CaCuoc{
     
     uint public amount = 0 ether;
-    uint quay = 5;
+  
+  
+    uint quay = 3;
     struct Customer{
         uint vis;
         address owner;
@@ -16,16 +18,20 @@ contract CaCuoc{
     ownerToNumber[] ownertonumbers2;
     uint[] numberS;
     
-    mapping(address => uint) ownerToUserId;
-    mapping(uint => address[]) numberToOwner;
+    mapping(address => uint) playerToNumber;
+    mapping(uint => address[]) numberToPlayers;
+    
+    
+
+    
     
     function DangKy() public{
-        require(ownerToUserId[msg.sender]==0x0,"Bạn đã đăng ký rồi");
+        require(playerToNumber[msg.sender]==0x0,"Bạn đã đăng ký rồi");
         uint8 id = uint8(customers.push(Customer(0, msg.sender)));
-        ownerToUserId[msg.sender] = id;
+        playerToNumber[msg.sender] = id;
     }
     function ChonSo(uint _number) public payable{
-        require(ownerToUserId[msg.sender]!=0x0,"Bạn phải đăng ký trước");
+        require(playerToNumber[msg.sender]!=0x0,"Bạn phải đăng ký trước");
         bool oke = true;
         for (uint8 i = 0; i < ownertonumbers.length; i++){
             if(ownertonumbers[i].owner == msg.sender){
@@ -34,29 +40,34 @@ contract CaCuoc{
         }
         require(oke==true,"Vui lòng đợi đến khi trao thưởng");
         require(msg.value >= 1 ether,"Tối thiểu 1 ether");
+        if(msg.value>1 ether){
+            msg.sender.transfer(msg.value-1 ether);
+        }
+        // nhap nhieu hon 1 ether van se chi tru 1 ether 
         ownertonumbers.push(ownerToNumber(msg.sender));
         amount +=msg.value;
-        numberToOwner[_number].push(msg.sender);
+        numberToPlayers[_number].push(msg.sender);
         numberS.push(_number);
         if(ownertonumbers.length%quay==0){
             QuayThuong();
         }
     }
-    
+ 
     function QuayThuong() private {
-        uint random =  (uint(keccak256(now)) % quay) + 1;
-        address[] adr = numberToOwner[random];
+        uint random  =  (uint(keccak256(now)) % quay + 1);
+        address[] memory  adr = numberToPlayers[random];
         if(adr.length>0){
-            uint tienthuong = amount/adr.length;
+            uint totalBet = amount/adr.length;
             for (uint8 i = 0; i< adr.length; i++){
                 for (uint8 i2 = 0; i2 < customers.length; i2++){
                     if(customers[i2].owner == adr[i]){
-                        customers[i2].vis +=tienthuong;
+                        customers[i2].vis +=totalBet;
+                        customers[i2].owner.transfer(totalBet);
                     }
                 }
             }
             for (uint8 i3 = 0; i3 < numberS.length; i3++){
-                delete numberToOwner[numberS[i3]];
+                delete numberToPlayers[numberS[i3]];
             }
             amount = 0 ether;
             delete numberS;
@@ -66,4 +77,8 @@ contract CaCuoc{
             ownertonumbers = ownertonumbers2;
         }
     }
+   
+    
+    
+    
 }
