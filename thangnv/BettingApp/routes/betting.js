804e32380e5bb35d6977035972210e0ba2829a73
vinsofts-thangnv <thangnv@vinsofts.net>
2018-11-07 17:34:03 +0700

    update express generator

---
+++ thangnv/BettingApp/routes/betting.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var betting= require('../contracts/betting/bettingContract');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('betting/index', { title: 'Express' });
+});
+
+router.post('/api/register', (req, res, next)=>{
+  betting.register(req.body.privateKey,req.body.number, req.body.money*req.body.unit).then(data=>{
+    res.send(data);
+  });
+})
+
+router.get('/api/history',(req,res, next)=>{
+  betting.getHistory().then(data=> res.send(data));
+})
+  
+router.get('/api/infobetting',(req,res, next)=>{   
+  betting.getInfoBetting().then(data=> res.send(data));
+})
+
+module.exports = router;
